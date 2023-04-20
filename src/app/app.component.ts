f27aa4302cd1108fbac13699fbbe885681449ee4
Irvin Dominguez <al169890@alumnos.uacj.mx>
2023-04-19 22:40:10 -0600

    modulo autocompletar y calendario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Query } from '@angular/core';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'compprime';
+
+  titulo:any;
+ suggestions:any[] = [];
+ selectedItem: any;
+ items:any[] = [];
+ date: Date = new Date();
+
+  upper() {
+    this.titulo = this.titulo.toUpperCase();
+  }
+
+  
+
+  search(event:any) {
+    this.suggestions = [...Array(11).keys()].map(item => event.query + '-' + item);
+}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ButtonModule } from 'primeng/button'; 
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { AppComponent } from './app.component';
-
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    ButtonModule
+    ButtonModule,
+    InputTextModule,
+    FormsModule,
+    AutoCompleteModule,
+    CalendarModule,
+    DropdownModule,
+    BrowserAnimationsModule  
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
